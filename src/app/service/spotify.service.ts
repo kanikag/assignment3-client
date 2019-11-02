71603bdae05295a96a05432f31aa0dd9a278d674
kanika gulabani <kanika.gulabani@konsult.atg.se>
2019-11-02 23:52:01 +0530

    commit

--- src/app/service/spotify.service.ts
+++ src/app/service/spotify.service.ts
@@ -12,7 +12,7 @@ export class SpotifyService {
   login() {
     const authorizationTokenUrl = 'https://accounts.spotify.com/authorize';
     const client_id = '46c4c2487589493e9435f73e216c58bf';
-    const parameters = '?client_id=' + client_id + '&redirect_uri=https://damp-coast-42712.herokuapp.com/login&scope=user-read-private%20user-read-email&response_type=token&state=123';
+    const parameters = '?client_id=' + client_id + '&redirect_uri=https://intense-oasis-86040.herokuapp.com/login&scope=user-read-private%20user-read-email&response_type=token&state=123';
 
     window.location.href = authorizationTokenUrl + parameters;
   }
